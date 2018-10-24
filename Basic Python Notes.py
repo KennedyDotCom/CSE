e4e212bfb14ce6e0d35d5a0bbe3bf2dd7aa8a1a8
KennedyDotCom <44372252+KennedyDotCom@users.noreply.github.com>
2018-10-24 08:45:07 -0700

    Added more Print statements

---
+++ Basic Python Notes.py
@@ -0,0 +1,41 @@
+print("Hello World!")
+print()
+# This is a comment . I can write whatever I want
+# Here and it won't do anything about it.
+# It has no effect on the code.
+print()  # This adds extra spaces spaces on the terminal
+print("This will print here. Notice the spacing")
+
+a = 4
+b = 3
+print(a + b)
+print(5 - 3)
+print(6 / 5)  # Results in a float
+print(a * 5)
+
+print("Figure thus out")
+print(6 // 4)
+print(12 // 3)  # Results in a integer
+print(9 // 4)
+
+car_name = "Thanos Car"
+car_type = "Tesla"
+car_cylinders = 4
+car_miles_per_gallon = 0.01
+print("I have a car called %s. Its pretty nice." % car_name)
+
+# MORE MATH STUFF
+print("Figure this stuff out too...")
+print(6 % 4)
+print(5 % 3)
+print(9 % 4)
+
+name = input("What is your name ")
+print("Hello %s" % name)
+
+real_age = int(input("How old are you? >_"))
+hidden_age = real_age + 5
+print(hidden_age)
+print("%d is incredibly old. you are actually %d old." % (hidden_age, real_age)
+
+age = input("How old are you?")
