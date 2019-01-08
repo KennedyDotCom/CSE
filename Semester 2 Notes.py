7491e2c87d98f84e7248088f96f2affe2adbd6b3
KennedyDotCom <44372252+KennedyDotCom@users.noreply.github.com>
2019-01-08 08:44:44 -0800

    Semester 2 Notes

---
+++ Semester 2 Notes.py
@@ -0,0 +1,21 @@
+print('Hello World')
+#  Single Line Comment
+cars = 5
+driving = True
+# cars = 5 → "5"
+
+print("I have %s cars" % cars)
+print("I have " + str(cars) + " cars")
+
+
+age = input("How old are you?")
+print(age + "? My Dad is older than you")
+
+colors = ["red", "topaz", "white", "orange", "purple"]
+colors.append("cyan")
+print(colors)
+
+colors.pop(0)
+print(colors)
+
+
