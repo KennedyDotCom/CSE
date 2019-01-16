c8386e985dfea280490ca4b11db2c4944a967523
KennedyDotCom <44372252+KennedyDotCom@users.noreply.github.com>
2019-01-16 08:43:10 -0800

    hangman

--- Hangman-Isaiah Vega.py
+++ Hangman-Isaiah Vega.py
@@ -1,13 +1,11 @@
 import string
 import random
 
-word_bank = ["smells", "it", "like", "Xbox", "is", "better", "than", "ps4", "AirPods", "Broke", "In", "here", 'sorry']
+word_bank = ["smells", "it", "like", "xbox", "is", "better", "than", "ps4", "AirPods", "Broke", "In", "here", 'sorry']
 letters = string.ascii_letters
 print(word_bank)
 word = random.choice(word_bank)
 print(word)
-
 print(list(string.ascii_letters))
 print(list(string.digits))
 print(list(string.punctuation))
-
