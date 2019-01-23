19526bf44f873cb92c76c3e8de834be98c3060ed
KennedyDotCom <44372252+KennedyDotCom@users.noreply.github.com>
2019-01-23 08:42:23 -0800

    HangMan

--- Hangman-Isaiah Vega.py
+++ Hangman-Isaiah Vega.py
@@ -1,8 +1,15 @@
 import string
 import random
 
-word_bank = ["smells", "it", "like", "xbox", "is", "better", "than", "ps4", "AirPods", "Broke", "In", "here", 'sorry']
+word_bank = ["Xbox", "how", "than", "ps4", "is", "today", "superior", "better", "help", "but", "pc", 'more']
 letters = string.ascii_letters
-print(word_bank)
 word = random.choice(word_bank)
 print(word)
+guesses_left = 8
+letters_guessed = []
+
+while guesses_left >= 1:
+    guessed_letter = input("Guess a letter: ")
+    letters_guessed.append(guessed_letter)
+    print(letters_guessed)
+    
