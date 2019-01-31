5e02a4aa2d89fd6f90e6e764855bb6747fc22e3d
KennedyDotCom <44372252+KennedyDotCom@users.noreply.github.com>
2019-01-31 08:45:22 -0800

    World Map

--- Dictionary World Map-Isaiah Vega.py
+++ Dictionary World Map-Isaiah Vega.py
@@ -7,14 +7,36 @@ world_map = {
             'NORTH': "PARKING_LOT"
 
         }
-    }
-},
-'PARKING_LOT': {
-    'NAME': "The Edison Parking Lot",
-    'DESCRIPTION': "There are cars parked here. To "
-                   "the south is Mr. Wiebe's room",
+    },
+    'PARKING_LOT': {
+        'NAME': "The Edison Parking Lot",
+        'DESCRIPTION': "There are cars parked here. To "
+                       "the south is Mr. Wiebe's room",
 
-    'PATH': {
-        'SOUTH': "R19A"
+        'PATH': {
+            'SOUTH': "R19A"
+        }
     }
 }
+
+
+# Other Variables
+
+current_node = world_map['R19A']
+directions = ['NORTH', 'SOUTH', 'EAST', 'WEST', 'UP', 'DOWN']
+playing = True
+
+# Controller
+while playing:
+    print(current_node('NAME'))
+    command = input(">_")
+    if command.lower() in ['q', 'quit', 'exit']:
+        playing = False
+    elif command.upper() in directions:
+        try:
+            room_name = current_node['PATHS'][command.upper()]
+        current_node = world_map[room_name]
+        except KeyError:
+        print("I can't go that way.")
+    else:
+       print("Command Not Recognized")
