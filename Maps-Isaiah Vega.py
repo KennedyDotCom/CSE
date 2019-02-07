3b384fd6284cb0812f8ba3f8a91dcf83721ed663
KennedyDotCom <44372252+KennedyDotCom@users.noreply.github.com>
2019-02-07 08:43:48 -0800

    Dictionary Map

--- Maps-Isaiah Vega.py
+++ Maps-Isaiah Vega.py
@@ -1,19 +1,31 @@
 coastline = {
     'Main Lobby': {
         'NAME': 'Main Lobby',
-        'DESCRIPTION': 'This is where this begins. '
-                       'Your Challenge is to find a defuser and disarm a bomb. '
+        'DESCRIPTION': 'This is where this begins. \n '
+                       'Your Challenge is to find a defuser and disarm a bomb. \n '
                        'There are Toiliets on one side and stairs on another',
         'PATHS': {
-            'NORTH': 'TOILETS'
-            'SOUTH'  'SOUTH STAIRS'
-            'WEST'   'SECURITY OFFICE'
+            'NORTH':  'TOILETS',
+            'SOUTH':  'SOUTH STAIRS',
+            'EAST':   'CANT GO THAT WAY!!',
+            'WEST':   'SECURITY OFFICE'
         }
     },
     'TOILETS': {
         'NAME': 'TOILETS',
-        'DESCRIPTION':
+        'DESCRIPTION': 'This is where you go to the bathroom. \n'
+                       'Why here you ask?\n'
+                       'Ahead of you is were you make or bake food',
+        'PATHS': {
+            'NORTH': 'You need something to break that wall',
+            'SOUTH': ''
+        }
+
+
+
+
+
+
     }
 
 }
-
