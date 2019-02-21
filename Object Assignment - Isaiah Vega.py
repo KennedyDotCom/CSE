cb610d41651082d668552804c2bf2f2320dfa74b
KennedyDotCom <44372252+KennedyDotCom@users.noreply.github.com>
2019-02-21 08:42:38 -0800

    OOP

--- Object Assignment - Isaiah Vega.py
+++ Object Assignment - Isaiah Vega.py
@@ -1,14 +1,20 @@
 class Microwave(object):
-    def __init__(self, buttons, handle=True, colors=Red, plate=True, light=True):
-        self.buttons
-        self.handle
-        self.color
-        self.plate
-        self.light
-
-        self.buttons = buttons
+    def __init__(self, handle=True, colors=False, plate=True, light=True):
+        self.buttons = True
         self.handle = handle
         self.color = colors
         self.plate = plate
         self.light = light
+        self.duration_of_time = 30
+
+    def heat(self, time):
+        if self.light:
+            if self.buttons <= 0:
+                print("No one hasn't started the microwave up")
+            elif self.duration_of_time < time:
+                print("You are done after %s", self.duration_of_time)
+                self.duration_of_time = 0
+            else:
+                print("You are done with %s time" % time)
+
 
