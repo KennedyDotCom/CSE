d2ba38f8f64079cec98a2bc5e5a53ff8785cbfdd
KennedyDotCom <44372252+KennedyDotCom@users.noreply.github.com>
2019-02-26 08:43:59 -0800

    Rooms

--- World Map OOP.py
+++ World Map OOP.py
@@ -7,6 +7,24 @@ class Room(object):
         self.description = description
 
 
+class Player(object):
+    def __init__(self, starting_location):
+        self.health = 100
+        self.current_location = starting_location
+        self.shield = 100
+        self.inventory = []
+        self.damage = 10
+
+    def move(self, newlocation):
+        """This method moves a character to a new location
+
+        :param newlocation: The variable containing a room object
+        """
+        self.current_location = newlocation
+
+
+
+
 R19A = Room("R19A")
 parking_lot = Room('The parking lot', None, R19A)
 
@@ -15,3 +33,27 @@ R19A.north = parking_lot
 
 R19A = Room("R19A", 'parking_lot')
 parking_lot = Room('The parking lot', None, R19A)
+
+player = Player(R19A)
+playing = True
+directions = ['north', 'south', 'east', 'west']
+
+while playing:
+    print(player.current_location.name)
+    print(player.current_location.description)
+    command = input(">_")
+    if command.lower() in ['q', 'quit', 'exit']:
+        playing = False
+    elif command.upper() in directions:
+        try:
+           # command = 'north'
+           room_object = getattr(player.current_location, command)
+
+           #NEEDED FOR OPTION 2
+           room_var = globals()[room_object]
+
+           player.move(room_object)
+        except KeyError:
+        print("I can't go that way.")
+    else:
+       print("Command Not Recognized")
